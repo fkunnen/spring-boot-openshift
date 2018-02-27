6aa4e562503ae2489346edd8b91efb306bba3645
Frank Kunnen <frank_kunnen@hotmail.com>
2018-02-27 10:47:47 +0100

    Spring boot skeleton

---
+++ imonit-api/src/main/java/com/cegeka/imonit/api/ImonitApiApplication.java
@@ -0,0 +1,12 @@
+package com.cegeka.imonit.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImonitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImonitApiApplication.class, args);
+	}
+}
